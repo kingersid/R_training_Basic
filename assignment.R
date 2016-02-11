7aee7a1fdf79d1e0911cbda63654e69edd823ede
siddharth kinger <kinger.siddharth@gmail.com>
2016-02-11 10:56:33 +0530

    Update assignment.R

--- assignment.R
+++ assignment.R
@@ -12,9 +12,9 @@ char <- data[c(2,3,4,5)]
 
 char
 # Ans3
-happy_loyal_business<-  data["Occupation"] == "Business" & data["Response"] == "Loyal" | (data["ACT1"]>5 & data["ACT2"]>5 & data["ACT4"]>5 & data["ACT5"]>5 & data["ACT10"]>5 & data["ACT14"]>5 & data["ACT16"]>5 & data["ACT17"]>5 & data["ACT24"]>5 & data["ACT26"]>5 & data["ACT28"]>5 & data["ACT30"]>5 & data["ACT31"]>5 & data["ACT32"]>5)
+happy_loyal_business<-  data["Occupation"] == "Business" & data["Response"] == "Loyal" | (data["ACT1"]>=5 & data["ACT2"]>=5 & data["ACT4"]>=5 & data["ACT5"]>=5 & data["ACT10"]>=5 & data["ACT14"]>=5 & data["ACT16"]>=5 & data["ACT17"]>=5 & data["ACT24"]>=5 & data["ACT26"]>=5 & data["ACT28"]>=5 & data["ACT30"]>=5 & data["ACT31"]>=5 & data["ACT32"]>=5)
 
-length(happy_loyal_business[happy_loyal_business==TRUE]) # ans : 111
+length(happy_loyal_business[happy_loyal_business==TRUE]) 
 
 # Ans 4
 class("Occupation")
@@ -37,8 +37,8 @@ dim(LoyalRank_mat) # Ans : 208x14
 Near_from_work <- data[data$Age=='20-30' & data$Sex=='Male' & data$WorkplaceProximity>=5, ]
 Near_from_home <- data[data$Age=='20-30' & data$Sex=='Female' & data$ResidenceProximity>=5, ]
 
-colnames(Near_from_home)[which(names(Near_from_home) == "WorkplaceProximity")] <- "Distance_rating"
-colnames(Near_from_work)[which(names(Near_from_work) == "ResidenceProximity")] <- "Distance_rating"
+colnames(Near_from_home)[which(names(Near_from_home) == "ResidenceProximity")] <- "Distance_rating"
+colnames(Near_from_work)[which(names(Near_from_work) == "WorkplaceProximity")] <- "Distance_rating"
 
 Distance_from_Bank <- rbind(Near_from_work,Near_from_home)
 Distance_from_Bank <- Distance_from_Bank[c("Branch","Response","Occupation", "Age","Sex","Distance")]
